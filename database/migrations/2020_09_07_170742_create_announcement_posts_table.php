3312989755d36e3b46237cbbe178bb2f46a8ae64
Loay Mohamed <loaymohamed1999@gmail.com>
2020-09-08 12:08:21 +0200

    Models Folder created and User moved to it

--- database/migrations/2020_09_07_170742_create_announcement_posts_table.php
+++ database/migrations/2020_09_07_170742_create_announcement_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AnnouncementPostsTable extends Migration
+class CreateAnnouncementPostsTable extends Migration
 {
     /**
      * Run the migrations.

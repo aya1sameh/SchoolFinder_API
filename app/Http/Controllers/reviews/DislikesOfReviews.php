52841dcd9c90e748feef2c7ca8b243f0469532f6
nardin1998 <nardinashraf1998@gmail.com>
2020-09-26 01:13:04 +0200

    count num /add/remove like and dislike on review

--- app/Http/Controllers/reviews/DislikesOfReviews.php
+++ app/Http/Controllers/reviews/DislikesOfReviews.php
@@ -5,80 +5,63 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 class DislikesOfReviews extends Controller
-{
+{ public function __construct()
+    {
+        $this->middleware('auth:api')->except(['numOfDislikes']);
+    }
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function numOfDislikes($id)
     {
-        //
+        $review=review::find($id);
+        if(is_null($review)){
+            return response()->json(["message"=>"This review is not found!"],404);
+
+         $dislikes = DislikesOfReview::where("review_id",$id)->count();
+           return response()->json($dislikes,200);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
+    
+      /**
+     * Add dislike.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function IncrementLikes(Request $request,$id)
     {
-        //
+    $review=review::find($id);
+        if(is_null($review)){
+            return response()->json(["message"=>"This review is not found!"],404);
+
+            $dislikes=DislikesOFReview::create($request->all());
+       $dislikes->user_id= $request->user()->id;
+          $dislikes->review_id= $id;
+       $dislikes->save();
+       return response()->json($dislikes,201);
+
+        
     }
 
+
     /**
-     * Display the specified resource.
+     * Remove dislike.
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function DecrementLikes(Request $request,$id1,$id2)
     {
-        //
-    }
+        $review=review::find($id);
+        if(is_null($review)){
+            return response()->json(["message"=>"This review is not found!"],404);
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
+            $dislikes=DislikesOfReview::find($id2);
+            $dislikes->delete();
+        return response()->json(null,204);
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
--- app/Http/Controllers/reviews/LikesOfReviews.php
+++ app/Http/Controllers/reviews/LikesOfReviews.php
@@ -5,80 +5,64 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 class LikesOfReviews extends Controller
-{
+{ public function __construct()
+    {
+        $this->middleware('auth:api')->except(['numOfLikes']);
+    }
     /**
-     * Display a listing of the resource.
+     * Display number of likes.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function numOfLikes($id)
     {
-        //
+        $review=review::find($id);
+        if(is_null($review)){
+            return response()->json(["message"=>"This review is not found!"],404);
+
+         $likes = LikesOfReview::where("review_id",$id)->count();
+           return response()->json($likes,200);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+  
 
     /**
-     * Store a newly created resource in storage.
+     * Add like.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function IncrementLikes(Request $request,$id)
     {
-        //
+    $review=review::find($id);
+        if(is_null($review)){
+            return response()->json(["message"=>"This review is not found!"],404);
+
+            $likes=LikesOFReview::create($request->all());
+       $likes->user_id= $request->user()->id;
+          $likes->review_id= $id;
+       $likes->save();
+       return response()->json($likes,201);
+
+        
     }
 
+
     /**
-     * Display the specified resource.
+     * Remove like.
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function DecrementLikes(Request $request,$id1,$id2)
     {
-        //
-    }
+        $review=review::find($id);
+        if(is_null($review)){
+            return response()->json(["message"=>"This review is not found!"],404);
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
+            $likes=LikesOfReview::find($id2);
+            $likes->delete();
+        return response()->json(null,204);
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }

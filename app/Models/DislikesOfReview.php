3b7f1f5aa9b75eed1b94598f90284ab8b150fd04
nardin1998 <nardinashraf1998@gmail.com>
2020-09-25 23:06:32 +0200

    recreated tables , models and controllers of likes and dislikes of reviews

---
+++ app/Models/DislikesOfReview.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DislikesOfReview extends Model
+{
+    protected $fillable=['user_id','review_id'];
+     public function users()
+    {
+        return $this->belongsTo('App\Models\User');
+    }
+
+    public function reviews()
+    {
+        return $this->belongsTo('App\Models\review');
+    }
+}
